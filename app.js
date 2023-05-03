c822e07d74edf6c213f3c006c112d351922136aa
Israel <israel.klein@jjay.cuny.edu>
2023-05-03 10:26:53 -0400

    added log mechanism

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from 'path'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
+import fs from 'fs'
 
 import {getUser} from './database.js';
 
@@ -13,16 +14,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"))
 app.get('/', async (req,res) => {
+    let time = new Date()
+    
+    let ip = req.ip;
+    let protocol = req.protocol
+    let method = req.method
+    let data = `${time}, ${ip}, ${protocol}, ${method} \n`;
+    fs.appendFile('output.txt',data, (err) => {if (err) throw err})
     res.sendFile(path.join(__dirname, "public", 'views', "index.html"))
     // res.send(path.join("hello"))
 })
 
 app.post('/submit', async (req,res) => {
     console.log(req.body);
+
+    let time = new Date()
+    let ip = req.ip;
+    let protocol = req.protocol
+    let method = req.method
+    let email_field = req.body.email;
+    let password_field = req.body.password
+    let data = `${time}, ${ip}, ${protocol}, ${method}, ${email_field}, ${password_field}, \n`;
+    fs.appendFile('form_log.txt',data, (err) => {if (err) throw err})
+
     const user = await getUser(req.body);
-    // console.log(user)
     res.send(user)
-    // res.send(path.join("hello"))
 })
+
+
  
 app.listen(3000)
