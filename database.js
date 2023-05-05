b20d5514170a1a0a2fe39f3d8b62e698b842fc0c
Israel <israel.klein@jjay.cuny.edu>
2023-05-05 12:34:49 -0400

    added comments

--- database.js
+++ database.js
@@ -3,34 +3,32 @@ import dotenv from 'dotenv';
 import crypto from 'crypto';
 
 dotenv.config()
-
+// Creates pool for connections
 const pool = mysql.createPool({
     host:process.env.HOST,
     user:process.env.USER,
     password:process.env.PASSWORD,
     database:process.env.DATABASE
 }).promise()
-
+// Requests user info from database using the email and hashed password as parameters
 export async function getUser(reqBody) {
-    console.log(reqBody)
-    console.log(reqBody.email)
-    console.log(reqBody.password)
     const email = reqBody.email;
     const password = crypto.createHash('sha512').update(reqBody.password).digest('hex');
-
-    console.log(typeof(email))
-    console.log(typeof(password))
+    // query without vulnerability
 //     const query = 
 //     `SELECT 
 //         u.fname, u.lname, u.email, u._password, u.ssn
 //     FROM 
 //         sqli.users as u
 //     WHERE 
-//         email = ?`
-       
+//         email = ?
+//     AND 
+//         _password = ?
+//      `
 //    console.log(query)
-//     const [rows] = await pool.query(query,[email]); 
+//     const [rows] = await pool.query(query,[email,password]); 
 
+    // Query with vulnerability 
     const [rows] = await pool.query(
         `
         SELECT 
